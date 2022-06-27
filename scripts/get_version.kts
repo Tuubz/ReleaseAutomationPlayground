e3905e80606a63b19b6e03aab08bf87070da9ab5
Tobias (Toby) Heuts <tobias@nimblehq.co>
2022-06-27 17:19:47 +0700

    Merge pull request #231 from nimblehq/release/3.8.0

--- scripts/get_version.kts
+++ scripts/get_version.kts
@@ -1,10 +1,9 @@
 import java.io.File
 
 object GetVersion {
-
     fun search(args: Array<String>): String? {
         val commitMessage = args.first()
-        val version = commitMessage.matches("/(?<=release\\/).[^\\s]+/g")
+        val version = commitMessage.substringAfterLast("/")
         return version
     }
 }
